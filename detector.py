23b4b8977b06bdd04cf8127ab9fd417505680c3e
Deeparth Gupta <deeparth.g@rayreachtech.com>
2020-12-15 19:07:17 +0530

    Modified detector but nothing to output to yet. Added to gitignore.

--- detector.py
+++ detector.py
@@ -1,3 +1,4 @@
+from os import system
 import numpy as np
 from cv2 import cv2
 
@@ -10,19 +11,20 @@ def load_yolo():
         classes = [line.strip() for line in f.readlines()]
     layers_names = net.getLayerNames()
     output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-    return net, classes, colors, output_layers
+    return net, classes, output_layers
 
 def detect_objects(img, net, outputLayers):		
-    #Use blobFromImage that accepts image/frame from video or webcam stream, model and output layers as parameters.	
-
+    #Use blobFromImage that accepts image/frame from video or webcam stream, model and output layers as parameters.
+    	
     blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
     net.setInput(blob)
     outputs = net.forward(outputLayers)
-    return blob, outputs
+    return outputs
 
 def get_box_dimensions(outputs, height, width):
-    #The list scores is created which stores the confidence corresponding to each object. Can play around with this value.
+    #The list scores is created which stores the confidence corresponding to each object. 
+    # Can play around with this value.
+    
     confidence = 0.5
     boxes = []
     confs = []
@@ -44,3 +46,34 @@ def get_box_dimensions(outputs, height, width):
                 confs.append(float(conf))
                 class_ids.append(class_id)
     return boxes, confs, class_ids
+
+def draw_labels(boxes, confs, class_ids, classes, img): 
+    #Draw bounding box and add object labels to it.
+    indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            cv2.rectangle(img, (x,y), (x+w, y+h), thickness=2)
+            cv2.putText(img, label, (x, y - 5), font, 1,thickness=1)
+    
+    return img
+
+def run_detection(input,output):    
+    #currently
+    
+    model, classes, output_layers = load_yolo()
+    cap = cv2.VideoCapture(input)
+    
+    while True:
+        if not output:
+            print('ERROR: Nothing to output to')
+            exit(1)
+        
+        _,frame = cap.read()
+        height, width, channels = frame.shape
+        outputs = detect_objects(frame, model, output_layers)
+        boxes, confidence, class_ids = get_box_dimensions(outputs,height,width)
+        
+        final_img = draw_labels(boxes,confidence,class_ids,classes,frame)
