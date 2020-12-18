29b095bcb1204d0c2d10bbf56925b223aedab37b
Deeparth Gupta <deeparth.g@rayreachtech.com>
2020-12-18 11:13:55 +0530

    Changed detector class to process single frames. Expanded gui.

--- gui.py
+++ gui.py
@@ -1,1 +1,22 @@
 import PySimpleGUI as gui
+import detector as det
+
+
+layout = [[gui.Image(filename='', key='videoOut',tooltip='Video goes here.', size=(480,320))],
+          [gui.ReadButton(button_text='Play',key='playbtn'), gui.Exit()]]
+
+window = gui.Window('Camera View')
+window.Layout(layout).finalize()
+
+stopped = 0
+
+
+
+while True:
+    event, values = window.Read(timeout=0)
+    if event == gui.WIN_CLOSED or event == 'Exit':
+        break
+    
+    
+    
+window.close()
