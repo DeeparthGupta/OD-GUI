5191dde784e294d931988db2eb783164353d563e
Deeparth Gupta <deeparth.g@rayreachtech.com>
2020-12-24 12:56:12 +0530

    Clean detector code.

--- detector.py
+++ detector.py
@@ -1,7 +1,6 @@
 import numpy as np
 from cv2 import cv2
 
-
 class Detector:
     
     def __init__(self, support_dir, size=(320,320)):
@@ -70,16 +69,8 @@ class Detector:
         return img
 
     def run_detection(self, frame, min_confidence = 0.5):    
-    # Accept a frame, run detection on it and draw a box around the detection.
+    # Accept a frame, run detection on it and draw a box around the detection
 
-        # cap = cv2.VideoCapture(input)
-        
-        """ while True:
-            if not output:
-                print('ERROR: Nothing to output to')
-                exit(1) """
-            
-        # _,frame = cap.read()
         height, width = frame.shape[:1]
         outputs = self.detect_objects(frame, self.model, self.output_layers)
         boxes, confidence, class_ids = self.get_box_dimensions(outputs,height,width,min_confidence)
