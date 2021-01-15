36f8a1918f182d3d048533f26f702454c0bb68b5
Deeparth Gupta <deeparth.g@rayreachtech.com>
2021-01-15 11:21:23 +0530

    Added contrast equalization.

--- detector.py
+++ detector.py
@@ -4,13 +4,14 @@ from cv2 import cv2
 
 class Detector:
 # Class that runs object detection on an input frame.
-    def __init__(self, support_dir, size=(320,320)):
+    def __init__(self, support_dir, size=(320,320),equalize=True):
         self.yolo_dir = support_dir
         self.size = size
         self.model, self.classes, self.output_layers = self.load_yolo()        
+        self.clahe = cv2.createCLAHE(clipLimit=2.0) if equalize else None
+   
     
-    def load_yolo(self):
-    # Load the yolov3 weights and config file with the help of dnn module of openCV.
+    def load_yolo(self):# Load the yolov3 weights and config file with the help of dnn module of openCV.
         weights = self.yolo_dir + 'yolov3.weights'
         config = self.yolo_dir + 'yolov3.cfg'
         names = self.yolo_dir + 'coco.names'
@@ -26,16 +27,16 @@ class Detector:
         layers_names = net.getLayerNames()
         output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
         return net, classes, output_layers
+    
 
-    def detect_objects(self,img, net, outputLayers):		
-    # Use blobFromImage that accepts image/frame from video or webcam stream, model and output layers as parameters.          
+    def detect_objects(self,img, net, outputLayers):# Use blobFromImage that accepts image/frame from video or webcam stream, model and output layers as parameters.          
         blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=self.size, mean=(0, 0, 0), swapRB=True, crop=False)
         net.setInput(blob)
         outputs = net.forward(outputLayers)
         return outputs
+    
 
-    def get_box_dimensions(outputs, height, width, threshold):
-    # The list scores is created which stores the confidence corresponding to each object.     
+    def get_box_dimensions(self,outputs, height, width, threshold):# The list scores is created which stores the confidence corresponding to each object.     
         boxes = []
         confs = []
         class_ids = []
@@ -58,9 +59,9 @@ class Detector:
                     class_ids.append(class_id)
 
         return boxes, confs, class_ids
+    
 
-    def draw_labels(boxes, confs, class_ids, classes, img): 
-    # Draw bounding box and add object labels to it.
+    def draw_labels(self,boxes, confs, class_ids, classes, img):# Draw bounding box and add object labels to it.
         indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
         font = cv2.FONT_HERSHEY_PLAIN
         for i in range(len(boxes)):
@@ -72,10 +73,10 @@ class Detector:
         
         return img
 
-    def run_detection(self, frame, threshold = 0.5):    
-    # Accept a frame, run detection on it and draw a box around the detection.
+
+    def run_detection(self, frame, threshold = 0.5):# Accept a frame, run detection on it and draw a box around the detection.
         height, width = frame.shape[:1]
-        outputs = self.detect_objects(frame, self.model, self.output_layers)
+        outputs = self.detect_objects(self.clahe.apply(frame) if self.clahe else frame, self.model, self.output_layers)
         boxes, confidence, class_ids = self.get_box_dimensions(outputs,height,width,threshold)
             
         return self.draw_labels(boxes,confidence,class_ids,self.classes,frame)
